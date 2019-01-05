25d59d0d69be8b599f44729e803b8c2ad689f785
terry00123 <terry00123@gmail.com>
2019-01-05 18:35:45 +0900

    메인화면 수정

--- app/src/main/java/com/example/q/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/q/myapplication/LoginActivity.java
@@ -1,7 +1,80 @@
+package com.example.q.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 
+public class LoginActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.q.myapplication.MESSAGE";
+    CallbackManager callbackManager;
+    LoginButton loginButton;
 
-public class LoginActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                GraphRequestAsyncTask request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        LoginManager.getInstance().logOut();
+                    }
+                }).executeAsync();
+
+                Toast.makeText(getApplicationContext(), "Login success", Toast.LENGTH_SHORT).show();
+                String UID = accessToken.getUserId();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, UID);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void onClick(View v) {
+        if (v == findViewById(R.id.fb)) {
+            loginButton.performClick();
+        }
+    }
 }
